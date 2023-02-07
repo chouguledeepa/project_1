1ab935e152f074371f8134945a85bc39a70535b1
chouguledeepa <chouguledeepa99@gmail.com>
2023-02-07 10:22:14 +0530

    first commit

---
+++ core/management/commands/wait_for_db.py
@@ -0,0 +1,37 @@
+"""
+Django command to wait for database to be available
+"""
+
+######################################
+#topic : How to create custom management command in django
+#######################################
+"""
+how to create custom management commannds?
+how to create command similar to python
+how to create command django-admin start-app
+"""
+########################################
+import time
+from MySQLdb import Error
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def handle(self, *args, **kwargs):
+        """ Entrypoint for command """
+
+        self.stdout.write("waiting for database....")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Error, OperationalError):
+                self.stdout.write(
+                    "Database unavailable, waiting for 1 second..."
+                )
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
